4872e7d20a4812a4611e49be5225fac4a6900ea5
Shaul1KR <shaulikr07@gmail.com>
2024-11-09 13:51:32 +0200

    Use tailwind adn react inifite scroll component lib

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
-  return;
+  const [maxNumber, setMaxNumber] = useState<number>(60);
+  const fetchMoreData = () => {
+    setMaxNumber((prev) => prev + 20);
+  };
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={maxNumber}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div></div>
+        {Array.from({ length: maxNumber }).map((_, i) => (
+          <div className="border-2 w-8 h-8 ">{i}</div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
 }
 
 export default App;
